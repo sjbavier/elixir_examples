27f35efe3381c9e902621dded83fc5a4908c148c
sjbavier <sjbavier@gmail.com>
2023-05-21 22:39:10 -0400

    examples of built-in enum functions

--- lib/list_functions.ex
+++ lib/list_functions.ex
@@ -1,4 +1,5 @@
 defmodule ListFunctions do
+  # examples of the Enum built-ins
   def each([], _function), do nil
   def each([head | tail], function) do
     function.(head)
@@ -8,5 +9,19 @@ defmodule ListFunctions do
   def map([], _function), do nil
   def map([head | tail], function) do
     [function.(head) | map(tail, function)]
+  end
+
+  def reduce([], acc, _function), do: acc
+  def reduce([head | tail], acc, function) do
+    reduce(tail, function.(head, acc), function)
+  end
+
+  def filter([], _function), do: []
+  def filter([head | tail], function) do
+    if function.(head) do
+      [head | filter(tail, function)]
+    else
+      filter(tail, function)
+    end
   end
 end
